71ae39af51e845104f8e742bad5f68ea9e66ad98
azran <azranohad@gmail.com>
2020-10-21 10:12:00 +0300

    Initial commit

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,88 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: write your ID and name here
+ */
+#include <iostream>
+#include <math.h>
+#include "anomaly_detection_util.h"
+
+float avg(float* x, int size){
+    if (size == 0) {
+        return 0;
+    }
+    float sumValue = 0;
+    for (int i = 0; i < size; i++) {
+        sumValue = sumValue + x[i];
+    }
+	return sumValue / size;
+}
+
+// returns the variance of X and Y
+float var(float* x, int size){
+    if (size == 0) {
+        return 0;
+    }
+    float u = avg(x, size);
+    float sumVarPow = 0;
+
+    for (int i = 0; i < size; i++) {
+        float diffPow = pow(x[i] - u, 2);
+        sumVarPow = sumVarPow + diffPow;
+    }
+	return sumVarPow/size;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+    if (size == 0) {
+        return 0;
+    }
+    float multArr[size];
+    for (int i = 0; i < size; i++) {
+        multArr[i] = x[i] * y[i];
+    }
+    return (avg(multArr, size) - ((avg(x, size)) * avg(y,size)));
+}
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+    if (size == 0) {
+        return 0;
+    }
+    float multStdv = sqrt(var(x, size)) * sqrt(var(y, size));
+    return cov(x, y, size) / multStdv;
+}
+
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size){
+    float x[size];
+    float y[size];
+    for (int i = 0; i < size; i++) {
+        x[i] = points[i]->x;
+        y[i] = points[i]->y;
+    }
+    float a = (cov(x, y, size)) / var(x, size);
+    float b = avg(y, size) - a * avg(x, size);
+
+	return Line(a,b);
+}
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size){
+    if (size == 0) {
+        return 0;
+    }
+    Line line = linear_reg(points, size);
+    return dev(p, line);
+}
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l){
+    float f_x = l.a * p.x + l.b;
+    return abs(f_x - p.y);
+}
+
+
+
+
---
+++ cmake-build-debug/CMakeFiles/Anomaly_Detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Anomaly_Detector"
+  "Anomaly_Detector.pdb"
+  "CMakeFiles/Anomaly_Detector.dir/MainTrain.cpp.o"
+  "CMakeFiles/Anomaly_Detector.dir/anomaly_detection_util.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Anomaly_Detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
